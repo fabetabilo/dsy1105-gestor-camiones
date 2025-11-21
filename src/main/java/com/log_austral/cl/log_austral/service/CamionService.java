a015da4c10c43b95c5c62642cb52aa4c0864fa5a
fabetabilo <fab.tabilo@duocuc.cl>
2025-11-21 17:05:51 -0300

    service metodos

--- src/main/java/com/log_austral/cl/log_austral/service/CamionService.java
+++ src/main/java/com/log_austral/cl/log_austral/service/CamionService.java
@@ -1,5 +1,35 @@
 package com.log_austral.cl.log_austral.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.log_austral.cl.log_austral.model.Camion;
+import com.log_austral.cl.log_austral.repository.CamionRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class CamionService {
 
+    @Autowired
+    private CamionRepository camionRepository;
+
+    public Camion findById(Integer id) {
+        return this.camionRepository.findById(id).get();
+    }
+
+    public List<Camion> findAll() {
+        return this.camionRepository.findAll();
+    }
+
+    public Camion saveCamion(Camion camion) { // tambien funciona para PUT (actualizar)
+        return this.camionRepository.save(camion);
+    }
+
+    public void deleteById(Integer id) {
+        this.camionRepository.deleteById(id);
+    }
 }
