7d2160bb4385b55b5f8564ca98ce3f8eba1b04ea
fabetabilo <fab.tabilo@duocuc.cl>
2025-11-21 16:33:23 -0300

    primer commit

---
+++ src/main/java/com/log_austral/cl/log_austral/LogAustralApplication.java
@@ -0,0 +1,13 @@
+package com.log_austral.cl.log_austral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAustralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAustralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/log_austral/cl/log_austral/LogAustralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log_austral.cl.log_austral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAustralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
